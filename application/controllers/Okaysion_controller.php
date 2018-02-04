e6d708ad4bbc38de16e875e9da4c6ceafc892e37
Vincent Tibubos <greenchay22@gmail.com>
2018-02-04 10:02:19 -0800

    Okaysion controller has been updated

--- application/controllers/Okaysion_controller.php
+++ application/controllers/Okaysion_controller.php
@@ -5,5 +5,10 @@ class Okaysion_controller extends CI_Controller {
 		$this->load->view('templates/header');
 		$this->load->view('pages/home');
 		$this->load->view('templates/footer');
+	}public function view($page='home')
+	{
+		$this->load->view('templates/header');
+		$this->load->view('pages/'.$page);
+		$this->load->view('templates/footer');
 	}
 }
