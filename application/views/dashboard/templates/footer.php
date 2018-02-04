d5133ea2b4f68620d72933baac29fe3e721bd4dc
Vincent Tibubos <greenchay22@gmail.com>
2018-02-04 10:00:14 -0800

    views for dashboard has been added

---
+++ application/views/dashboard/templates/footer.php
@@ -0,0 +1,40 @@
+
+          <footer class="main-footer">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-sm-6">
+                  <p>OKAYsion &copy; 2018</p>
+                </div>
+
+               <!--
+ <header class="page-header">
+            <div class="container-fluid">
+              <h2 class="no-margin-bottom">Hi Admin!</h2>
+            </div>
+          </header>
+
+                <div class="col-sm-6 text-right">
+                  <p>Design by <a href="https://bootstrapious.com/admin-templates" class="external">Bootstrapious</a></p>
+                  <!-- Please do not remove the backlink to us unless you support further theme's 
+				  development at https://bootstrapious.com/donate. It is part of the license conditions.
+				  Thank you for understanding :)
+                </div>-->
+              </div>
+            </div>
+          </footer>
+          
+        </div>
+      </div>
+    </div>
+    <!-- Javascript files-->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo base_url();?>/assets/lib/jquery/jquery.min.js"> </script>
+    <script src="<?php echo base_url();?>/assets/dashboard/vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="<?php echo base_url();?>/assets/dashboard/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>/assets/dashboard/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="<?php echo base_url();?>/assets/dashboard/vendor/chart.js/Chart.min.js"></script>
+    <script src="<?php echo base_url();?>/assets/dashboard/vendor/jquery-validation/jquery.validate.min.js"></script>
+    <!-- Main File-->
+    <script src="<?php echo base_url();?>/assets/dashboard/js/front.js"></script>
+  </body>
+</html>
