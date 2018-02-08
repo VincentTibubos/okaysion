1f896c4dcb06dc0efa7434f24f2e66a6965b26ad
Vincent Tibubos <greenchay22@gmail.com>
2018-02-08 17:00:16 +0800

    session added to login and register

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,10 +2,18 @@
 	class dashboard extends CI_Controller{
 		public function register(){
 
+            if($this->session->userdata('logged_in')){
+                redirect();
+            }
+
 			echo "hi ".$_POST['email']." your password is ".$_POST['pass'];
 
 		}
 		public function signin(){
+
+            if($this->session->userdata('logged_in')){
+                redirect();
+            }
 			$this->form_validation->set_rules('email','Email','trim|required');
 			$this->form_validation->set_rules('email','Email','trim|required|callback_checklogin');
 			if($this->form_validation->run()==false){
@@ -17,6 +25,9 @@
 
 		}
 		public function view($page='index'){
+            if(!$this->session->userdata('logged_in')){
+                redirect('login');
+            }
 			$this->load->view('dashboard/templates/header');
 			$this->load->view('dashboard/'.$page);
 			$this->load->view('dashboard/templates/footer');
