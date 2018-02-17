b04e7a1899fd9ef477bc096254f4bd29b6e42bb9
Vincent Tibubos <greenchay22@gmail.com>
2018-02-17 09:49:45 +0800

    update logout confirmation modal added

--- application/views/dashboard/templates/footer.php
+++ application/views/dashboard/templates/footer.php
@@ -26,6 +26,30 @@
         </div>
       </div>
     </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="logoutm" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Logout</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <p>Do you realy want to logout?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-success" data-dismiss="modal"  onclick="redirout();">Yes</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+
     <!-- Javascript files-->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="<?php echo base_url();?>/assets/lib/jquery/jquery.min.js"> </script>
@@ -36,5 +60,11 @@
     <script src="<?php echo base_url();?>/assets/dashboard/vendor/jquery-validation/jquery.validate.min.js"></script>
     <!-- Main File-->
     <script src="<?php echo base_url();?>/assets/dashboard/js/front.js"></script>
+
+    <script>
+      function redirout(){
+              window.location="<?php echo base_url()?>logout";
+      }
+    </script>
   </body>
 </html>
